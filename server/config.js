485eed073f34fa37e3fe680eddab0b7f5b45afe1
Michael P. Orecchia <michael.orecchia@gmail.com>
2016-12-13 17:09:17 -0800

    add api config file

---
+++ server/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  token: '', // your discogs API token here
+  headers: {
+      'Accept': 'application/vnd.discogs.v2.plaintext+json',
+      'User-Agent': '' // your custom User Agent string here
+  },
+  username: '' // your discogs username here
+}
--- server/router.js
+++ server/router.js
@@ -1,18 +1,14 @@
 const request = require('request');
+const config = require('./config');
 
 const apiBase = 'https://api.discogs.com';
-const token = 'dkNuuUEtAEjdSyZiTLTplyGLIbYFujOZJgvMrYLH';
-const headers = {
-    'Accept': 'application/vnd.discogs.v2.plaintext+json',
-    'User-Agent': 'TestDiscogClient/0.1 +http://tonefolder.com'
-};
 
 var Router = function(app) {
     this.start = function() {
         app.route('/api/wantlist').get((req, res) => {
             const options = {
-                url: `${apiBase}/users/bedhed3000/wants?sort=added&sort_order=desc&token=${token}`,
-                headers: headers
+                url: `${apiBase}/users/${config.username}/wants?sort=added&sort_order=desc&token=${config.token}`,
+                headers: config.headers
             };
             request.get(options, getCallback(res));
         });
@@ -20,8 +16,8 @@ var Router = function(app) {
         app.route('/api/releases/:id').get((req, res) => {
             const id = req.params.id;
             const options = {
-                url: `${apiBase}/releases/${id}?token=${token}`,
-                headers: headers
+                url: `${apiBase}/releases/${id}?token=${config.token}`,
+                headers: config.headers
             };
             request.get(options, getCallback(res));
         });
@@ -29,8 +25,8 @@ var Router = function(app) {
         app.route('/api/artists/:id').get((req, res) => {
             const id = req.params.id;
             const options = {
-                url: `${apiBase}/artists/${id}?token=${token}`,
-                headers: headers
+                url: `${apiBase}/artists/${id}?token=${config.token}`,
+                headers: config.headers
             };
             request.get(options, getCallback(res));
         });
@@ -38,24 +34,24 @@ var Router = function(app) {
         app.route('/api/labels/:id').get((req, res) => {
             const id = req.params.id;
             const options = {
-                url: `${apiBase}/labels/${id}?token=${token}`,
-                headers: headers
+                url: `${apiBase}/labels/${id}?token=${config.token}`,
+                headers: config.headers
             };
             request.get(options, getCallback(res));
         });
 
         app.route('/api/collection').get((req, res) => {
             const options = {
-                url: `${apiBase}/users/bedhed3000/collection/folders/0/releases?sort=added&sort_order=desc&token=${token}`,
-                headers: headers
+                url: `${apiBase}/users/${config.username}/collection/folders/0/releases?sort=added&sort_order=desc&token=${config.token}`,
+                headers: config.headers
             };
             request.get(options, getCallback(res));
         });
 
         app.route('/api/inventory').get((req, res) => {
             const options = {
-                url: `${apiBase}/users/bedhed3000/inventory?sort=listed&sort_order=desc&token=${token}`,
-                headers: headers
+                url: `${apiBase}/users/${config.username}/inventory?sort=listed&sort_order=desc&token=${config.token}`,
+                headers: config.headers
             };
             request.get(options, getCallback(res));
         });
@@ -63,8 +59,8 @@ var Router = function(app) {
         app.route('/api/listing/:id').get((req, res) => {
             const id = req.params.id;
             const options = {
-                url: `${apiBase}/marketplace/listings/${id}?curr_abbr=USD&token=${token}`,
-                headers: headers
+                url: `${apiBase}/marketplace/listings/${id}?curr_abbr=USD&token=${config.token}`,
+                headers: config.headers
             };
             request.get(options, getCallback(res));
         });
