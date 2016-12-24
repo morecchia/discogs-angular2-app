c0f9723612ee209b1fa40bade4387ed4be756c03
Michael P. Orecchia <michael.orecchia@gmail.com>
2016-12-23 17:05:35 -0800

    fix bottom margin when no video loaded

--- client/app/app.component.ts
+++ client/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Router  } from '@angular/router';
 
+import { Subscription }   from 'rxjs/Subscription';
+
 import { LocalStorageService } from 'angular-2-local-storage';
 
 import { YoutubeService } from '../services/youtube.service';
@@ -13,9 +15,16 @@ import { YoutubeService } from '../services/youtube.service';
 export class AppComponent implements OnInit {
   title = 'Discogs Player';
   activeVideo: any;
+  activeVideoSubscription: Subscription;
 
   constructor(private router: Router, private youtube: YoutubeService,
-    private localStorage: LocalStorageService ) {
+    private localStorage: LocalStorageService) {
+      this.activeVideoSubscription = youtube.videoActivated$
+        .subscribe(video => {
+          this.activeVideo = video;
+          this.localStorage.set('activeVideo', video);
+        });
+
       const lastVideo = this.localStorage.get('activeVideo');
       this.activeVideo = lastVideo;
   }
